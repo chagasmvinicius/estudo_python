3256ceaa3b72a8c888b66f0e0b7d16f038c1d0e7
Vinicius <vmc.mart@gmail.com>
2022-09-11 18:14:06 -0300

    Alteração do .inset e .pop pelo pela atribuição direto no [index]

--- comecando_com_a_linguagem/forca.py
+++ comecando_com_a_linguagem/forca.py
@@ -11,17 +11,20 @@ def jogar():
     acertou = False
     enforcou = False
 
+    print(f'Palavra: {letras_acertadas}')
+
     while not acertou and not enforcou:
         chute = input("Digite uma letra: ").strip()
 
         index = 0
         for letra in palavra_secreta:
             if chute.lower() == letra.lower():
-                letras_acertadas.insert(index, letra)
-                letras_acertadas.pop(index + 1)
-                print(letras_acertadas)
+                #letras_acertadas.insert(index, letra)
+                #letras_acertadas.pop(index + 1)
+                # OU... \/
+                letras_acertadas[index] = letra
             index = index + 1
-
+        print(letras_acertadas)
 
 if __name__ == '__main__':
-        jogar()
+    jogar()
