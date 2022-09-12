1b403613a9584841ec3bb74bc1d3956fa5a44f80
Vinicius <vmc.mart@gmail.com>
2022-09-12 09:48:27 -0300

    Semi finalização do jogo forca.py + list comprehensions

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Documentação Python 3: https://docs.python.org/3/tutorial/
 🎖 CERTIFICADOS:
 
 [CURSO] PYTHON: COMENÇANDO COM A LINGUAGEM: https://cursos.alura.com.br/certificate/94cae4ff-e32c-420d-9a16-8ed7f9e4b2b1
+
+📝 APRENDIZADOS DIFERENTES:
+
+List Comprehensions: https://pythonacademy.com.br/blog/list-comprehensions-no-python
+Ex.: letras_acertadas = ['_' for index in palavra_secreta]
--- comecando_com_a_linguagem/forca.py
+++ comecando_com_a_linguagem/forca.py
@@ -3,28 +3,51 @@ def jogar():
     print('####### BEM VINDO AO JOGO FORCA ########')
     print('########################################\n')
 
-    palavra_secreta = 'banana'
-    letras_acertadas = []
-    for index in palavra_secreta:
-        letras_acertadas.append('_')
+    palavra_secreta = ('banana').upper()
+    
+    # letras_acertadas = []
+    # for index in palavra_secreta:
+    #     letras_acertadas.append('_')
+    
+    # OU... \/
+    # letras_acertadas = ['_'] * len(palavra_secreta)
+    
+    # OU... \/ (list comprehensions)
+    letras_acertadas = ['_' for index in palavra_secreta]
 
     acertou = False
     enforcou = False
+    erros = 0
+    tentativas = 6
 
     print(f'Palavra: {letras_acertadas}')
 
     while not acertou and not enforcou:
-        chute = input("Digite uma letra: ").strip()
+        chute = input("Digite uma letra: ").strip().upper()
+        print('\n')
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute.lower() == letra.lower():
-                #letras_acertadas.insert(index, letra)
-                #letras_acertadas.pop(index + 1)
-                # OU... \/
-                letras_acertadas[index] = letra
-            index = index + 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    #letras_acertadas.insert(index, letra)
+                    #letras_acertadas.pop(index + 1)
+                    # OU... \/
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print(f'Chute com a letra errada! Faltam {tentativas - erros} tentativas!\n')
+        
+        enforcou = erros == tentativas
+        acertou = '_' not in letras_acertadas        
+        
         print(letras_acertadas)
+    
+    if acertou:
+        print('\nG A N H O U!\nFim de jogo!')
+    else:
+        print('\nP E R D E U!\nFim de jogo!')
 
 if __name__ == '__main__':
     jogar()
